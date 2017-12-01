f9f704557ad9bf1e608813f833086846f37a7632
pakorn <pakorn@kitchenup.com>
2017-12-01 16:20:11 +0700

    Add umbrella

---
+++ M80AttributedLabel/M80AttributedLabel-umbrella.h
@@ -0,0 +1,9 @@
+#import <UIKit/UIKit.h>
+#import <CoreText/CoreText.h>
+#import <Foundation/Foundation.h>
+
+#import "M80AttributedLabelDefines.h"
+#import "M80AttributedLabel.h"
+#import "M80AttributedLabelAttachment.h"
+#import "M80AttributedLabelURL.h"
+#import "NSMutableAttributedString+M80.h"
